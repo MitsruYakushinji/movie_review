286276c5b6cf78275d3e3408a29a9ca6cabc5d77
MitsruYakushinji <156178833+MitsruYakushinji@users.noreply.github.com>
2024-01-25 10:30:02 +0900

    Update MovieForm.java

--- src/main/java/com/example/form/MovieForm.java
+++ src/main/java/com/example/form/MovieForm.java
@@ -1,24 +1,19 @@
 package com.example.form;
 
-//リクエストの送受信
 public class MovieForm {
 	private String title;
 	private Integer score;
 
-	// titleのアクセサー
 	public String getTitle() {
 		return this.title;
 	}
-
 	public void setTitle(String title) {
 		this.title = title;
 	}
 
-	// scoreのアクセサー
 	public Integer getScore() {
 		return this.score;
 	}
-
 	public void setScore(Integer score) {
 		this.score = score;
 	}
